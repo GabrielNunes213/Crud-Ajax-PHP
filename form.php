b05dc3b65392bfc5f1fbd62893fdd13c3b116d0b
GabrielNunes213 <gabrielvieira.n06@gmail.com>
2022-11-09 21:33:39 -0300

    form

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" id="nome" name="nome" placeholder="Digite seu nome"/>
+        <br>
+        <input type="text" id="email" name="nome" placeholder="Digite seu email"/>
+        <br>
+        <input type="button" id="btn_gravar" value="Salvar"/>
+        
+    </form>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
